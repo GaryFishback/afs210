7a02ae2a3fb7f74ce867e9d72491dbbec5e3c528
GaryFishback <gary.joel.fishback@gmail.com>
2021-02-13 15:11:14 -0600

    hashing

--- week4/hashing/hashing.py
+++ week4/hashing/hashing.py
@@ -1,6 +1,6 @@
 int_val = 4
 str_val = 'PythonIsBest'
-flt_val = hash(24.56)
+flt_val = 24.56
 
 print(f'The integer hash value is: {hash(int_val)}')
 print(f'The string hash value is: {hash(str_val)}')
