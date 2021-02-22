d0018bbabe55fd11ebbe6d486ddad1d3b93ceeea
GaryFishback <gary.joel.fishback@gmail.com>
2021-02-22 01:35:56 -0600

    Merge and count

--- week5/countingsort/count.py
+++ week5/countingsort/count.py
@@ -1,1 +1,16 @@
-# https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-search-and-sorting-exercise-10.php
+def counting_sort(passedArray, highest_num):
+    top = highest_num + 1
+    count = [0] * top
+
+    for a in passedArray:
+        count[a] += 1
+    i = 0
+    for a in range(top):
+        for c in range(count[a]):
+            passedArray[i] = a
+            i += 1
+    return passedArray
+
+print(counting_sort( [1, 2, 7, 3, 2, 1, 4, 2, 3, 2, 1], 7 ))
+# print(counting_sort([1, 2, 7, 3, 2, 1, 4, 2, 3, 2, 1, 9], 7))
+
