3aa0302f96388a6ed2987c261475a8fb6bf92791
GaryFishback <gary.joel.fishback@gmail.com>
2021-03-01 00:01:11 -0600

    week6 compelte

--- week6/algorithms/algorithm.py
+++ week6/algorithms/algorithm.py
@@ -1,22 +1,18 @@
-# Initilize denominator 
 k = 1
   
-# Initilize sum 
-s = 0
+pi = 0
   
 for i in range(1000000): 
-  
-    # even index elements are positive 
+
     if i % 2 == 0:
-        s += 4/k
+        pi += 4/k
 
     else: 
   
-        # odd index elements are negative 
-        s -= 4/k 
-  
-    # denominator is odd 
+
+        pi -= 4/k 
+
     k += 2
-decimalPoint = int(input("To what decimal point?"))
-newvalue = f"{{:.{decimalPoint}f}}".format(s)
-print(newvalue) 
+decimalPoint = int(input("Enter the number of decimals to calculate to: "))
+newPi = f"{{:.{decimalPoint}f}}".format(pi)
+print(newPi) 
